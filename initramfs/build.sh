e85f26ff50b2abdc0bf209659917189ad1d37538
Ubuntu <ubuntu@ip-172-31-34-207.eu-west-1.compute.internal>
2016-01-18 14:33:38 +0000

    trying, trying to get working...

--- initramfs/build.sh
+++ initramfs/build.sh
@@ -61,22 +61,15 @@ bundle /bin/sh
 bundle /bin/dash
 bundle /bin/mount
 bundle_libs /bin/mount
-bundle /lib/x86_64-linux-gnu/libc.so.6
-bundle /lib/x86_64-linux-gnu/ld-2.21.so
 bundle /lib64/ld-linux-x86-64.so.2
 bundle /sbin/fsck
 bundle /sbin/fsck.ext3
 bundle /sbin/fsck.ext4
-bundle /lib/modules/`uname -r`/kernel/fs/fuse/cuse.ko
-bundle /lib/modules/`uname -r`/kernel/drivers/net/ethernet/intel/e1000/e1000.ko
-bundle /lib/modules/`uname -r`/modules.dep
 bundle /lib/systemd/systemd-udevd
 bundle_libs /lib/systemd/systemd-udevd
 bundle /bin/udevadm
 bundle /sbin/udevadm
 bundle_libs /bin/udevadm
-
-cp /usr/lib/PXELINUX/pxelinux.0 $BUILD_DIR
 
 mkdir -p $BUILD_DIR/usr/bin
 mkdir -p $BUILD_DIR/sys
--- initramfs/ec2-build.sh
+++ initramfs/ec2-build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export EC2_AMITOOL_HOME=/usr/local/ec2/ec2-ami-tools-1.5.7
-export PATH=$EC2_AMITOOL_HOME/bin:/usr/sbin:$PATH
-
 . creds.sh
 
 set -e
@@ -13,31 +10,19 @@ then
     exec sudo -E $0
 fi
 
-RELEASE=`date '+%F'`
+N=1
+RELEASE=`date '+%F'`-$N
 
-BUILD_DIR=`mktemp -d -p /var/tmp`
 SOURCE_DIR=`pwd`
 IMAGES_DIR=`mktemp -d -p /var/tmp`
 
-cd $BUILD_DIR
-gzip -dc $SOURCE_DIR/uvwpmacoh.cpio.gz | cpio -iv
-#cp $SOURCE_DIR/vmlinuz $BUILD_DIR/
-
-SIZE=100 # `du -m $BUILD_DIR | awk '{print int($1 * 1.5)}'`
-
-mkdir -p $BUILD_DIR/{boot/grub,usr/sbin}
-cp -av $SOURCE_DIR/grub.conf $BUILD_DIR/boot/grub/
-cp -av /usr/sbin/grub $BUILD_DIR/usr/sbin/
-
-ec2-bundle-vol \
+ec2-bundle-image \
     -c $SOURCE_DIR/cert.pem \
     -k $SOURCE_DIR/pk.pem \
     -u 694960934408 \
+    -i $SOURCE_DIR/fs \
     -r x86_64 \
     --block-device-mapping ami=sda1,root=/dev/sda1 \
-    -s $SIZE \
-    --no-inherit \
-    -v $BUILD_DIR \
     -d $IMAGES_DIR \
     --kernel aki-4feec43b
 
@@ -46,15 +31,15 @@ ec2-upload-bundle \
     -d $IMAGES_DIR \
     --access-key "$AWS_ACCESS_KEY" \
     --secret-key "$AWS_SECRET_KEY" \
-    --manifest $IMAGES_DIR/image.manifest.xml \
+    --manifest $IMAGES_DIR/fs.manifest.xml \
     --location eu-west-1 \
     --acl public-read
 
 ec2-register \
     --verbose --debug \
-    uvwpmacoh-images2/bundles/uvwpmacoh-$RELEASE/image.manifest.xml \
+    uvwpmacoh-images2/bundles/uvwpmacoh-$RELEASE/fs.manifest.xml \
     -n uvwpmacoh-$RELEASE \
     -O "$AWS_ACCESS_KEY" \
     -W "$AWS_SECRET_KEY" \
-    --virtualization-type hvm \
+    --virtualization-type paravirtual \
     --region eu-west-1
